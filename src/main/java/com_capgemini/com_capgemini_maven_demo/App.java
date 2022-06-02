0122ac58cdc670f11a77c530a0a690891b72e08a
saisreepoleboina <saishreepoleboina1999@gmail.com>
2022-06-02 10:27:44 +0530

    Initial commit

---
+++ src/main/java/com_capgemini/com_capgemini_maven_demo/App.java
@@ -0,0 +1,13 @@
+package com_capgemini.com_capgemini_maven_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
